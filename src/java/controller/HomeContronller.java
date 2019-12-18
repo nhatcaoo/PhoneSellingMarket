5e856e36970987412fa3d515a241603b2249fdec
nhatcaoo <39726840+nhatcaoo@users.noreply.github.com>
2019-12-19 03:44:25 +0700

    a

--- src/java/controller/HomeContronller.java
+++ src/java/controller/HomeContronller.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author This PC
  */
-public class loo extends HttpServlet {
+public class HomeContronller extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,16 +31,7 @@ public class loo extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet loo</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet loo at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
         }
     }
 
